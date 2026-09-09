3b897cf7af12e08631c64430a31818ac09879043
sythm7 <minematt31@gmail.com>
2026-09-09 11:35:53 +0200

    Added start script and moved Docker related files to docker-app folder

---
+++ start.sh
@@ -0,0 +1,16 @@
+docker_name=monapp
+
+ps=`docker ps -a | grep $docker_name`
+
+image=`echo $ps | awk '{print $2}'`
+
+container_id=`echo $ps | awk '{print $1}'`
+
+if [ "$image" = "$docker_name" ]
+then
+    docker start $container_id
+else
+    docker build -t $docker_name ./docker-app
+    docker run $docker_name
+fi
+    
