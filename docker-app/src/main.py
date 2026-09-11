4151e3cab32ac41df4df49c8668d67458935ea8a
sythm7 <minematt31@gmail.com>
2026-09-11 11:26:37 +0200

    Created basic post

--- docker-app/src/main.py
+++ docker-app/src/main.py
@@ -6,3 +6,7 @@ app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 @app.get("/users/{username}")
 async def read_user(username: str):
     return {"message": f"Hello {username}"}
+
+@app.post("/logs")
+async def post_log(log: str) :
+    return "Ton log '" + log + "' a bien été post"
